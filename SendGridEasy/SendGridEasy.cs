eb223969be84f86b2fa21799df66b6eb96792d10
Weber <andrew.weber@us.sogeti.com>
2016-06-21 16:52:31 -0400

    initial files for project

---
+++ SendGridEasy/SendGridEasy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendGridEasy
+{
+    public class SendGridEasy
+    {
+        public SendGridEasy()
+        {
+            
+        }
+
+        //1 recipient constructor params:
+        //string of from email (email + name)
+        //string of recipient email
+        //string of subject
+        //string of message body
+        public SendGridEasy(string sgFromEmail, string sgToEmail, string sgSubject, string sgMessageBody)
+        {
+            
+        }
+
+        //multiple recipient constructor params:
+        //string of from email (email + name)
+        //string[] of recipient emails
+        //string of subject
+        //string of message body
+        public SendGridEasy(string sgFromEmail, string[] sgToEmail, string sgSubject, string sgMessageBody)
+        {
+
+        }
+
+
+        private void sgConfig()
+        {
+            //configure sendgrid credentials here
+        }
+
+        private void sgSendMessage()
+        {
+            
+        }
+    }
+}
---
+++ SendGridEasy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SendGridEasy.Startup))]
+namespace SendGridEasy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
